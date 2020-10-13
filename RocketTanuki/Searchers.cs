b29c53998e2e00034f0043895de6a31b10b004f1
nodchip <nodchip@gmail.com>
2020-10-13 10:30:37 +0900

    bestmove出力中にクラッシュする可能性があるバグを修正した

--- RocketTanuki/Searchers.cs
+++ RocketTanuki/Searchers.cs
@@ -77,7 +77,7 @@ namespace RocketTanuki
             {
                 writer.Write("resign");
             }
-            if (bestMove.Next != null && bestMove.Next.Move != Move.Resign && bestMove.Next.Move != Move.Win)
+            if (bestMove.Next != null && bestMove.Next.Move != null && bestMove.Next.Move != Move.Resign && bestMove.Next.Move != Move.Win)
             {
                 writer.Write(" ponder ");
                 writer.Write(bestMove.Next.Move.ToUsiString());
