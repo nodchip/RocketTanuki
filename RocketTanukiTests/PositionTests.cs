0f5f796d8c295d4096b795d619cb19b7a66904af
nodchip <nodchip@gmail.com>
2022-03-10 22:38:21 +0900

    ハッシュ値が正しく計算できないバグを修正した

--- RocketTanukiTests/PositionTests.cs
+++ RocketTanukiTests/PositionTests.cs
@@ -4,65 +4,82 @@ using RocketTanuki;
 
 namespace RocketTanukiTests
 {
-    [TestClass]
-    public class PositionTests
-    {
-        [TestInitialize]
-        public void Initialize()
-        {
-            Program.Initialize();
-        }
+	[TestClass]
+	public class PositionTests
+	{
+		[TestInitialize]
+		public void Initialize()
+		{
+			Program.Initialize();
+		}
 
-        [TestMethod]
-        public void Position_Hash()
-        {
-            foreach (var sfen in new[] { Position.StartposSfen, Position.MatsuriSfen, Position.MaxSfen })
-            {
-                var position = new Position();
-                position.Set(sfen);
+		[TestMethod]
+		public void Position_Hash()
+		{
+			foreach (var sfen in new[] { Position.StartposSfen, Position.MatsuriSfen, Position.MaxSfen })
+			{
+				var position = new Position();
+				position.Set(sfen);
 
-                long hash = position.Hash;
-                foreach (var move in MoveGenerator.Generate(position, null))
-                {
-                    using (var mover = new Mover(position, move)) { }
-                    Assert.AreEqual(hash, position.Hash);
-                }
-            }
-        }
+				long hash = position.Hash;
+				foreach (var move in MoveGenerator.Generate(position, null))
+				{
+					using (var mover = new Mover(position, move)) { }
+					Assert.AreEqual(hash, position.Hash);
+				}
+			}
+		}
 
-        [TestMethod]
-        public void Position_King()
-        {
-            var position = new Position();
-            position.Set(Position.StartposSfen);
+		[TestMethod]
+		public void Position_Hash_Sennichite()
+		{
+			var position = new Position();
+			position.Set(Position.StartposSfen);
+			var expected = position.Hash;
 
-            Assert.AreEqual(4, position.BlackKingFile);
-            Assert.AreEqual(8, position.BlackKingRank);
-            Assert.AreEqual(4, position.WhiteKingFile);
-            Assert.AreEqual(0, position.WhiteKingRank);
-        }
+			var moves = new[] { "5i5h", "5a5b", "5h5i", "5b5a" };
+			foreach (var move in moves)
+			{
+				position.DoMove(Move.FromUsiString(position, move));
+			}
 
-        [TestMethod]
-        public void Position_ToSfenString()
-        {
-            foreach (var expected in new[] { Position.StartposSfen, Position.MatsuriSfen, Position.MaxSfen })
-            {
-                var position = new Position();
-                position.Set(expected);
-                var actual = position.ToSfenString();
-                Assert.AreEqual(expected, actual);
-            }
-        }
+			var actual = position.Hash;
+			Assert.AreEqual(expected, actual);
+		}
 
-        [TestMethod]
-        public void GenerateMove_OnlyCapture()
-        {
-            var position = new Position();
-            position.Set(Position.MatsuriSfen);
-            foreach (var move in MoveGenerator.Generate(position, null, 0, 0))
-            {
-                Assert.AreNotEqual(Piece.NoPiece, move.PieceTo);
-            }
-        }
-    }
+		[TestMethod]
+		public void Position_King()
+		{
+			var position = new Position();
+			position.Set(Position.StartposSfen);
+
+			Assert.AreEqual(4, position.BlackKingFile);
+			Assert.AreEqual(8, position.BlackKingRank);
+			Assert.AreEqual(4, position.WhiteKingFile);
+			Assert.AreEqual(0, position.WhiteKingRank);
+		}
+
+		[TestMethod]
+		public void Position_ToSfenString()
+		{
+			foreach (var expected in new[] { Position.StartposSfen, Position.MatsuriSfen, Position.MaxSfen })
+			{
+				var position = new Position();
+				position.Set(expected);
+				var actual = position.ToSfenString();
+				Assert.AreEqual(expected, actual);
+			}
+		}
+
+		[TestMethod]
+		public void GenerateMove_OnlyCapture()
+		{
+			var position = new Position();
+			position.Set(Position.MatsuriSfen);
+			foreach (var move in MoveGenerator.Generate(position, null, 0, 0))
+			{
+				Assert.AreNotEqual(Piece.NoPiece, move.PieceTo);
+			}
+		}
+	}
 }
